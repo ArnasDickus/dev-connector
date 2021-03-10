7f951f5a88a65ae374c4412b9581d9b5e11aeb58
Arnas Dickus <arnasdickus@protonmail.com>
2021-03-10 08:38:42 +0200

    client adding

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <h2>React app</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/pages/contacts/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            Home Page 2
+        </div>
+    )
+}
+
+export default HomePage;
